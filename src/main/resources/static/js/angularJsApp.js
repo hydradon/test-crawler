a14a1ce44130ff75e589b02806dec257a2d40595
hydradon <hijacker1092@yahoo.com>
2019-03-24 21:04:11 +0800

    Include angular sanitize library to convert link in text to make them clickable

--- src/main/resources/static/js/angularJsApp.js
+++ src/main/resources/static/js/angularJsApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngSanitize']);
 
 app.controller('jsaController', function($scope, $http, $location, $interval) {
     $scope.listAllTweets = [];
